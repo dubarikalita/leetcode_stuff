3a8b2239070239e9d9e5b849afe5db0d8773863b
Dubari Kalita <dubarikalita4114@gmail.com>
2026-05-18 10:39:23 +0530

    Time: 0 ms (100%), Space: 20.5 MB (98.47%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int reqdNum=nums[0];
+        int res=nums[0];
         for(int i=1;i<nums.size();i++){
-            reqdNum=reqdNum^nums[i];
+            res=res^nums[i];
         }
-        return reqdNum;
+        return res;
     }
 };
