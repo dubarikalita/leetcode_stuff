c419dea7c8ed034ccf3077212f5c1da8a8a91008
Dubari Kalita <dubarikalita4114@gmail.com>
2026-05-18 20:15:44 +0530

    Time: 0 ms (100%), Space: 17.5 MB (53.07%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,48 +1,45 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int first=firstOccurence(nums,target);
-        int last=lastOccurence(nums,target);
+        int first=firstOcc(nums,target);
+        int last=lastOcc(nums,target);
+        auto it=find(nums.begin(),nums.end(),target);
+        if( it==nums.end()) return {-1,-1};
+
         return {first,last};
     }
 
-    int firstOccurence(vector<int>& nums,int target){
-        int low=0;
-        int high=nums.size()-1;
-        int first=-1;
-        while(low<=high){
-            int mid=low+((high-low)/2);
-            if(nums[mid]==target){
-                first=mid;
-                high=mid-1;
-            }
-            else if(nums[mid]<target){
-                low=mid+1;
+    int firstOcc(vector<int> &nums,int target){
+        int l=0;
+        int h=nums.size()-1;
+        int ans=-1;
+        while(l<=h){
+            int m=l+(h-l)/2;
+            if(nums[m]>=target){
+                ans=m;
+                h=m-1;
             }
             else{
-                high=mid-1;
+                l=m+1;
             }
         }
-        return first;
+        return ans;
     }
 
-    int lastOccurence(vector<int>& nums,int target){
-        int low=0;
-        int high=nums.size()-1;
-        int last=-1;
-        while(low<=high){
-            int mid=low+((high-low)/2);
-            if(nums[mid]==target){
-                last=mid;
-                low=mid+1;
-            }
-            else if(nums[mid]<target){
-                low=mid+1;
+    int lastOcc(vector<int> &nums,int target){
+        int l=0;
+        int h=nums.size()-1;
+        int ans=-1;
+        while(l<=h){
+            int m=l+(h-l)/2;
+            if(nums[m]<=target){
+                ans=m;
+                l=m+1;
             }
             else{
-                high=mid-1;
+                h=m-1;
             }
         }
-        return last;
+        return ans;
     }
 };
