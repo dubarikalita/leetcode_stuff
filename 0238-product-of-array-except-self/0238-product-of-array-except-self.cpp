da881a6b8bf37737fe69ef348104e8b0b927cd52
Dubari Kalita <dubarikalita4114@gmail.com>
2026-09-06 11:32:19 +0530

    Time: 4 ms (20.96%), Space: 40.2 MB (81%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        
-        int n=nums.size();
-        vector<int>ans(n,1);
-
-        int left=1;
-        for(int i=0;i<n;i++){
-            ans[i]=left;
-            left*=nums[i];
+        vector<int>ans(nums.size(),1);
+        int prefix=1;
+        for(int i=0;i<nums.size();i++){
+            ans[i]=prefix;
+            prefix*=nums[i];
         }
-        int right=1;
-        for(int i=n-1;i>=0;i--){
-            ans[i]*=right;
-            right*=nums[i];
+        int suffix=1;
+        for(int i=nums.size()-1;i>=0;i--){
+            ans[i]*=suffix;
+            suffix*=nums[i];
         }
         return ans;
     }
