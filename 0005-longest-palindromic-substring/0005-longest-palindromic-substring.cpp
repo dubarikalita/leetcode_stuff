2e416b2b9a9bca903c4ba474aaa8b46b22122eda
Dubari Kalita <dubarikalita4114@gmail.com>
2026-09-06 16:21:21 +0530

    Time: 2 ms (98.65%), Space: 9.5 MB (57.63%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        if (s.empty()) return "";
+        
+        int start=0;
+        int maxLen=1;
 
-        int start =0;
-        int maxLength=1;
-
-        auto expand=[&](int left, int right){
-            while(left>= 0 && right <s.length() && s[left]==s[right]){
-                left--;
-                right++;
-            }
-            return right-left-1;
-        };
         for(int i=0;i<s.length();i++){
-            int len1=expand(i,i);
-            int len2=expand(i,i+1);
+
+            int len1=expand(s,i,i);
+            int len2=expand(s,i,i+1);
+
             int len=max(len1,len2);
 
-            if(len>maxLength){
-                maxLength=len;
-                start =i-(len-1)/2;
+            if(len>maxLen){
+                maxLen=len;
+                start=i-(len-1)/2;
             }
         }
-        return s.substr(start,maxLength);
+        return s.substr(start,maxLen);
+    }
 
+    int expand(string &s, int l, int r){
+        while(l>=0 && r<s.length() && s[l]==s[r]){
+            l--;
+            r++;
+        }
+        return r-l-1;
     }
 };
